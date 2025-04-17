84d0647dfb983c55fe915fe8aba9ebb5fb46c2c4
Maciej Pałka <maciej.palka123@gmail.com>
2025-04-17 19:11:51 +0200

    pusta aplikacja okienkowa

--- ui.py
+++ ui.py
@@ -1,13 +1,37 @@
-import tkinter
+import tkinter as tk  # for later
+import customtkinter as ctk
+from tkinter import *  # for later
+from customtkinter import filedialog  # for later
+import os  # for later
 
-import customtkinter
 
 class App:
-    def __init__(self):
-        self.root = tkinter.Tk()
-        self.root.title("Kalkulator")
-        self.frame = tkinter.Frame(self.root)
-        self.frame.pack()
+    def __init__(self, root):
+        # Inicjalizacja elementów
+        self.root = root
+        self.create_widgets()
+
+        root.title("Calculator")
+        root.resizable(height=False, width=False)
+
+    # Tworzenie widget'ów
+    def create_widgets(self):
+        padx_images = 10
+        ctk.set_appearance_mode("white")
+        ctk.set_default_color_theme("dark-blue")
 
 
-app = App()
+    # Funkcja tworząca etykietę
+    def create_label(self, parent, row, column, label_text):
+        label = ctk.CTkLabel(parent, text=f"{label_text} ", font=("Arial", 12))
+        label.grid(row=row, column=column, sticky="nw", columnspan=1, pady=2)
+        return label
+
+    # Funkcja aktualizująca wskazania dotyczące położenia slidera
+
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    root.geometry("320x500")
+    app = App(root)
+    root.mainloop()
