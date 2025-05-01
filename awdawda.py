b1e8d4e3d9fb730cb2c90ab01291ba6d7b5051e9
goo1iat <stasiek4023@gmail.com>
2025-05-01 10:20:35 +0200

    zapisz_operacje

--- awdawda.py
+++ awdawda.py
@@ -1,1 +1,21 @@
-print("Hello World")
+from collections import deque
+
+
+
+
+# ilość przechowywaneych działań w pamieci ,
+# działania wpisywane w postaci "2 + 2 = 4"
+# zapisywanie polega na wpisaniu komendy w poostaci jak w 21 linijce działania i wynik w osobnej
+history = deque(maxlen=10)
+
+def zapisz_operacje(dzialanie: str, wynik: float):
+
+    wpis = f"{dzialanie} = {wynik}"
+    history.append(wpis)
+
+
+
+
+
+
+zapisz_operacje("2 + 2", 4)
