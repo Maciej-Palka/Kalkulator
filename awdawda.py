c293c4cdb9161720b5da33c9b38d4221bf734e94
goo1iat <stasiek4023@gmail.com>
2025-05-08 12:48:11 +0200

    zapisz_historia

--- awdawda.py
+++ awdawda.py
@@ -13,9 +13,19 @@ def zapisz_operacje(dzialanie: str, wynik: float):
     wpis = f"{dzialanie} = {wynik}"
     history.append(wpis)
 
+def pokaz_historie():
+    if not history:
+        print("Brak zapisanych dzialan")
+        return
+    for i, wpis in enumerate(history,1):
+        print(f"{i}: {wpis}")
+    return
 
 
 
 
 
 zapisz_operacje("2 + 2", 4)
+zapisz_operacje("3 + 2", 3)
+zapisz_operacje("2 - 1", 2)
+pokaz_historie()
