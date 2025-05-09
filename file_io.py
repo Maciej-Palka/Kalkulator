a5f0f9f9a6d78e5cf3801e81d240f324f886b684
kpastula <kpastulanpg@gmail.com>
2025-05-09 23:35:26 +0200

    Utworzono plik file_io.py i zdefiniowano standard zapisu wyników: "[data czas] działanie = wynik"

---
+++ file_io.py
@@ -0,0 +1,14 @@
+# file_io.py
+
+"""
+Standard zapisu wyników do pliku:
+[data czas] działanie = wynik
+np. 2025-05-09 12:34:56 2 + 2 = 4
+"""
+
+def save_results_to_file(results, filename="results.txt"):
+    pass  # funkcja do zapisania wyników
+
+
+def load_results_from_file(filename="results.txt"):
+    pass  # funkcja do wczytania wyników
