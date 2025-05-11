00a193b31338be0286c273e3ad36b1417cc75363
kpastula <kpastulanpg@gmail.com>
2025-05-11 21:14:55 +0200

    Dodano funkcję save_results_to_file zapisującą wyniki do pliku .txt w ustalonym formacie

--- file_io.py
+++ file_io.py
@@ -1,4 +1,4 @@
-# file_io.py
+from datetime import datetime
 
 """
 Standard zapisu wyników do pliku:
@@ -7,8 +7,14 @@ np. 2025-05-09 12:34:56 2 + 2 = 4
 """
 
 def save_results_to_file(results, filename="results.txt"):
-    pass  # funkcja do zapisania wyników
+    try:
+        with open(filename, "w") as file:
+            for item in results:
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                file.write(f"{timestamp} {item['expression']} = {item['result']}\n")
+    except IOError as e:
+        print(f"Błąd zapisu do pliku: {e}")
 
 
 def load_results_from_file(filename="results.txt"):
-    pass  # funkcja do wczytania wyników
+    pass  # do zaimplementowania później
