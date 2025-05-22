396a33059c5c91da0bac565d294788e2946723e7
kpastula <kpastulanpg@gmail.com>
2025-05-22 20:49:37 +0200

    Dodano save_results_to_file

--- file_io.py
+++ file_io.py
@@ -6,16 +6,30 @@ Standard zapisu wyników do pliku:
 np. 2025-05-09 12:34:56 2 + 2 = 4
 """
 
+
+
+
+
+def save_results_to_file(results, filename="results.txt"):
+    try:
+        with open(filename, "w") as file:
+            for item in results:
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                file.write(f"{timestamp} {item['expression']} = {item['result']}\n")
+    except IOError as e:
+        print(f"Błąd zapisu do pliku: {e}")
+
+
 def load_results_from_file(filename="results.txt"):
     results = []
     try:
         with open(filename, "r") as file:
             for line in file:
                 line = line.strip()
-                if " = " not in line:
-                    continue  # pomiń niepoprawne linie
-
+                if " = " in line:
+                    continue
                 try:
+
                     date, time, rest = line.split(" ", 2)
                     expression, result = rest.split(" = ")
                     results.append({
@@ -26,20 +40,11 @@ def load_results_from_file(filename="results.txt"):
                 except ValueError:
                     print(f"Błąd parsowania linii: {line}")
                     continue
-
     except FileNotFoundError:
         print(f"Plik '{filename}' nie istnieje.")
     except Exception as e:
         print(f"Błąd podczas importu: {e}")
-
     return results
-
-
-if __name__ == "__main__":
-    test_results = [
-        {"expression": "3 + 3", "result": 6},
-        {"expression": "10 / 2", "result": 5}
-    ]
 
     save_results_to_file(test_results)
     loaded = load_results_from_file()
